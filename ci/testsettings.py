0c09cc46d75fd55b06e06d4697d3efaab11097f1
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-09 16:46:28 -0500

    Basic travis-ci build config and settings adapted from cdhweb
    [#227715553066836]

---
+++ ci/testsettings.py
@@ -0,0 +1,18 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+# If this file is changed in development, the development server will
+# have to be manually restarted because changes will not be noticed
+# immediately.
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'cdhweb.db'
+    }
+}
+
+# secret key added as a travis build step
+
