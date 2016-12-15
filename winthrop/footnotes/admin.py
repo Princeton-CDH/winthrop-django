3f17b746a6a1df32c55ebaf5dcf1b28ded788212
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-15 13:29:17 -0500

    Basic grappelli lookup to make footnote generic relation more usable

--- winthrop/footnotes/admin.py
+++ winthrop/footnotes/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import SourceType, Bibliography, Footnote
 
+class FootnoteAdmin(admin.ModelAdmin):
+    related_lookup_fields = {
+        'generic': [['content_type', 'object_id']]
+    }
+
+
 admin.site.register(SourceType)
 admin.site.register(Bibliography)
-admin.site.register(Footnote)
+admin.site.register(Footnote, FootnoteAdmin)
