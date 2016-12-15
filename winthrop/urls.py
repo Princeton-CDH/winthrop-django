1bb1fb84a2b331ff65f81fa9bfd74f4ab2f109dc
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-15 13:28:43 -0500

    Basic django-grappelli integration

--- winthrop/urls.py
+++ winthrop/urls.py
@@ -3,7 +3,7 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
@@ -11,5 +11,7 @@ urlpatterns = [
     # for now, since there is not yet any public-facing site,
     # redirect base url to admin index page
     url(r'^$', RedirectView.as_view(pattern_name='admin:index')),
+    # # grappelli URLS for admin related lookups & autocompletes
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
 ]
