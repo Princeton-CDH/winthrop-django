4fc6f34a6a14153e6824df23a3f87d50b523ce34
rlskoeser <rebecca.s.koeser@emory.edu>
2016-12-15 13:29:43 -0500

    Preliminary book admin customization for testing/user story development

--- winthrop/books/admin.py
+++ winthrop/books/admin.py
@@ -4,12 +4,23 @@ from .models import Subject, Language, Publisher, OwningInstitution, \
     Book, Catalogue, BookSubject, BookLanguage, CreatorType, Creator, \
     PersonBook
 
+class SubjectInline(admin.TabularInline):
+    model = BookSubject
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('short_title', 'publisher', 'pub_year', 'is_annotated',
+        'is_digitized', 'is_extant')
+    # TODO: order edit fields so notes displays last
+    inlines = [SubjectInline]
+    list_filter = ('subjects', 'languages')
+
 
 admin.site.register(Subject)
 admin.site.register(Language)
 admin.site.register(Publisher)
 admin.site.register(OwningInstitution)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Catalogue)
 admin.site.register(CreatorType)
 # NOTE: these will probably be inlines, but register for testing for now
