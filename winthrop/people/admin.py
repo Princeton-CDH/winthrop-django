d054180fe1ff5ff7f4a0bc5b62f8dfcbb15a9c09
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-01-25 09:34:42 -0500

    Add residence as an inline to person/people

--- winthrop/people/admin.py
+++ winthrop/people/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 from .models import Person, Residence, RelationshipType, Relationship
 
-admin.site.register(Person)
-admin.site.register(Residence)
+
+class ResidenceInline(admin.TabularInline):
+    '''Inline class for Residence'''
+    model = Residence
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [
+        ResidenceInline
+    ]
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(RelationshipType)
 admin.site.register(Relationship)
-
