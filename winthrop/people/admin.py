11b40f03baf85b9224f048ccaa14f3d5e7ad7737
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-01-26 10:50:59 -0500

    Commit and push changes for issues 18, 29, 20, 25

--- winthrop/people/admin.py
+++ winthrop/people/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Person, Residence, RelationshipType, Relationship
 
 
+class RelationshipInline(admin.TabularInline):
+    '''Inline class for Relationships'''
+    model = Relationship
+    fk_name = 'from_person'
+
+
 class ResidenceInline(admin.TabularInline):
     '''Inline class for Residence'''
     model = Residence
@@ -10,10 +16,11 @@ class ResidenceInline(admin.TabularInline):
 
 class PersonAdmin(admin.ModelAdmin):
     inlines = [
-        ResidenceInline
+        ResidenceInline, RelationshipInline
     ]
+    list_display = ('authorized_name', 'sort_name', 'viaf_id', 'family_group')
+    list_filter = ('family_group',)
 
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(RelationshipType)
-admin.site.register(Relationship)
