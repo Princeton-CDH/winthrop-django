0a8a77d30f6952d4cd748e7ba6aca9718fafc472
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-02-06 09:29:05 -0500

    Tweak django import so tests can pass under django 1.9

--- winthrop/places/tests.py
+++ winthrop/places/tests.py
@@ -2,7 +2,11 @@ from unittest.mock import patch
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 from django.test import TestCase, override_settings
-from django.urls import reverse
+try:
+    # django 1.10
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
 import json
 
 from .models import Place
