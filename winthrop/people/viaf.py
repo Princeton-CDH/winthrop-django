ad32c72d4ddd30f512f20d8a8712d523c1c299f3
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-02-10 14:43:10 -0500

    Simplify viaf lookup code and add a few more test cases

--- winthrop/people/viaf.py
+++ winthrop/people/viaf.py
@@ -1,19 +1,32 @@
-import json
 import requests
 
 
 class ViafAPI(object):
-    """Wrapper for Viaf API, results dict options for making JSON strings"""
+    """Wrapper for VIAF API.
 
-    def __init__(self):
-        self.base_url = "https://www.viaf.org/"
+    https://platform.worldcat.org/api-explorer/apis/VIAF
+    """
+
+    # NOTE: API urls use www prefix, but VIAF URIs do not
+
+    #: base url for VIAF API methods
+    api_base = "https://www.viaf.org/viaf"
+    #: base url for VIAF URIs
+    uri_base = "https://viaf.org/viaf"
 
     def suggest(self, query):
-        """Do a GET request to pull in JSON"""
-        url = self.base_url + "viaf/AutoSuggest?query="
-        r = requests.get("%s%s" % (url, query))
-        return (r.json())['result']
+        """Get VIAF suggestions for the specified query string.
+        For easy of processing, returns an empty list if no suggestions
+        are found or something goes wrong."""
+        url = '/'.join([self.api_base, 'AutoSuggest'])
+        resp = requests.get(url, params={'query': query})
+        # NOTE: could consider adding logging here if we find
+        # we are getting lots of unexpected errors
+        if resp.status_code == requests.codes.ok:
+            return resp.json().get('result', None) or []
+        return []
 
     @classmethod
     def uri_from_id(cls, viaf_id):
-        return "https://viaf.org/viaf/%s/" % viaf_id
+        """Generate a VIAF URI for the specified id"""
+        return "%s/%s/" % (cls.uri_base, viaf_id)
