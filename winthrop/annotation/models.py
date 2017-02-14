8ec68b135d915ac6a1e7a2c13df5133ebf8df72f
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-02-14 16:48:03 -0500

    Update for djiffy refactor to use manifest/canvas model names

--- winthrop/annotation/models.py
+++ winthrop/annotation/models.py
@@ -2,14 +2,14 @@ from urllib.parse import urlparse
 from django.db import models
 from django.urls import resolve, Resolver404
 from annotator_store.models import BaseAnnotation
-from djiffy.models import IfPage
+from djiffy.models import Canvas
 from winthrop.people.models import Person
 
 
 class Annotation(BaseAnnotation):
     # NOTE: do we want to associate explicitly with canvas in the db?
     # could just use uri, but faster lookup if we associate...
-    canvas = models.ForeignKey(IfPage, null=True, blank=True)
+    canvas = models.ForeignKey(Canvas, null=True, blank=True)
     author = models.ForeignKey(Person, null=True, blank=True)
 
 
@@ -19,13 +19,13 @@ class Annotation(BaseAnnotation):
         return info
 
     def save(self, *args, **kwargs):
-        # NOTE: could probably set the canvas uri in javascript instead
+        # NOTE: could set the canvas uri in javascript instead
         # of using page uri, but for now determine canvas id
         # based on the page uri
         try:
             match = resolve(urlparse(self.uri).path)
             if match.url_name == 'page' and 'djiffy' in match.namespaces:
-                self.canvas = IfPage.objects.get(
+                self.canvas = Canvas.objects.get(
                     short_id=match.kwargs['id'],
                     book__short_id=match.kwargs['book_id']
                 )
