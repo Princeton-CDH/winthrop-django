4848722b862291a69da7e3f35c1a1c3ed68a3711
bwhicks <benjamin.w.hicks@gmail.com>
2017-05-12 15:35:38 -0400

    Repoint default login to admin per #54

--- winthrop/urls.py
+++ winthrop/urls.py
@@ -15,7 +15,7 @@ from winthrop.annotation.views import TagAutocomplete
 urlpatterns = [
     # for now, since there is not yet any public-facing site,
     # redirect base url to admin index page
-    url(r'^$', RedirectView.as_view(url='/iiif-books/'), name='site-index'),
+    url(r'^$', RedirectView.as_view(pattern_name='admin:index'), name='site-index'),
     # # grappelli URLS for admin related lookups & autocompletes
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
