c29d2513073ad1ae8e22f2028316f2977fb300d0
bwhicks <benjamin.w.hicks@gmail.com>
2017-05-18 09:04:03 -0400

    Add help text to model and annotator field placeholder

--- winthrop/books/models.py
+++ winthrop/books/models.py
@@ -210,7 +210,11 @@ class PersonBook(Notable, DateRange):
     '''Interactions or connections between books and people other than
     annotation.'''
     # FIXME: better name? concept/thing/model
-    person = models.ForeignKey(Person)
+    person = models.ForeignKey(
+        Person,
+        help_text=('This association also determines if a person is added to '
+                   'the annotator autocomplete.')
+    )
     book = models.ForeignKey(Book)
     relationship_type = models.ForeignKey(PersonBookRelationshipType)
 
