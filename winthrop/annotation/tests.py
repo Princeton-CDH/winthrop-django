bc048be552d4c667ac3b37e7de7b0104c5cbf7df
bwhicks <benjamin.w.hicks@gmail.com>
2017-05-18 13:57:51 -0400

    Add tests for tags on handle_extra_data and info methods of Annotation

--- winthrop/annotation/tests.py
+++ winthrop/annotation/tests.py
@@ -1,5 +1,52 @@
 from django.test import TestCase
+from unittest.mock import Mock
+
 from .models import Annotation, Tag
+
+
+class TestAnnotation(TestCase):
+
+    def test_handle_extra_data(self):
+
+        ## -- test tag handling
+        annotation = Annotation.objects.create()
+        # must be saved to test related object handling
+
+        # test adding new tags
+        # - using three existing tags and one nonexistent
+        tags = ['manicule', 'underlining', 'dash', 'bogus']
+        data = annotation.handle_extra_data({'tags': tags},
+            Mock())   # NOTE: using Mock() for request - currently unused
+        # existing tags should be associated with annotation
+        assert annotation.tags.count() == 3
+        saved_tags = [tag.name for tag in annotation.tags.all()]
+        for tag in tags[:-1]:
+            assert tag in saved_tags
+        # bogus tag should be ignored
+        assert 'bogus' not in saved_tags
+        # tags should be removed from annotation extra data
+        assert 'tags' not in data
+
+        # test changing the list of existing tags
+        tags = ['manicule', 'dash']
+        annotation.handle_extra_data({'tags': tags}, Mock())
+        assert annotation.tags.count() == 2
+        # underlining should be removed
+        saved_tags = [tag.name for tag in annotation.tags.all()]
+        assert 'underlining' not in saved_tags
+
+        # test removing existing tags
+        annotation.handle_extra_data({'tags': []}, Mock())
+        assert annotation.tags.count() == 0
+
+    def test_info(self):
+        annotation = Annotation.objects.create()
+        # tags should be an empty list when none are set
+        assert not annotation.info()['tags']
+        tags = ['manicule', 'dash']
+        annotation.handle_extra_data({'tags': tags}, Mock())
+        # order not guaranteed / not important; sort for comparison
+        assert annotation.info()['tags'].sort() == tags.sort()
 
 
 class TestTag(TestCase):
