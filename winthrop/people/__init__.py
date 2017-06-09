880591df649972e893053d9b4c88ac55bb7dd624
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-06-08 22:20:49 -0400

    Clean up local module app configs

--- winthrop/people/__init__.py
+++ winthrop/people/__init__.py
@@ -3,3 +3,5 @@ Django app to track and display information about people associated with
 the Winthrop family library, including both Winthrop family members and
 associates as well as authors and editors of books in the library.
 '''
+
+default_app_config = 'winthrop.people.apps.PeopleConfig'
