6e3b305ae9d5a507066d9673cb9fc04134dea71d
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-07-18 16:23:32 -0400

    Clean up book view import for book urls

--- winthrop/books/urls.py
+++ winthrop/books/urls.py
@@ -1,21 +1,19 @@
 from django.conf.urls import url
 from django.contrib.admin.views.decorators import staff_member_required
 
-from winthrop.books.views import PublisherAutocomplete, CanvasAutocomplete, \
-    LanguageAutocomplete, SubjectAutocomplete, BookListView, BookDetailView, \
-    BookFacetJSONView
+from winthrop.books import views
 
 
 urlpatterns = [
-    url(r'^$', BookListView.as_view(), name='list'),
-    url(r'^facets/$', BookFacetJSONView.as_view(), name='facets'),
-    url(r'^(?P<slug>[-\w]+)/$', BookDetailView.as_view(), name='detail'),
-    url(r'^autocomplete/publisher/$', staff_member_required(PublisherAutocomplete.as_view()),
+    url(r'^$', views.BookListView.as_view(), name='list'),
+    url(r'^facets/$', views.BookFacetJSONView.as_view(), name='facets'),
+    url(r'^(?P<slug>[-\w]+)/$', views.BookDetailView.as_view(), name='detail'),
+    url(r'^autocomplete/publisher/$', staff_member_required(views.PublisherAutocomplete.as_view()),
         name='publisher-autocomplete'),
-    url(r'^autocomplete/canvas/$', staff_member_required(CanvasAutocomplete.as_view()),
+    url(r'^autocomplete/canvas/$', staff_member_required(views.CanvasAutocomplete.as_view()),
         name='canvas-autocomplete'),
-    url(r'^autocomplete/language/$', staff_member_required(LanguageAutocomplete.as_view()),
+    url(r'^autocomplete/language/$', staff_member_required(views.LanguageAutocomplete.as_view()),
         name='language-autocomplete'),
-    url(r'^autocomplete/subject/$', staff_member_required(SubjectAutocomplete.as_view()),
+    url(r'^autocomplete/subject/$', staff_member_required(views.SubjectAutocomplete.as_view()),
         name='subject-autocomplete'),
 ]
