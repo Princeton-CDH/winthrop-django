50e429175613800c0d5e54f6de5fdf814729a1ed
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-08-15 14:26:21 -0400

    Remove pagination information from json facet data

--- winthrop/books/views.py
+++ winthrop/books/views.py
@@ -229,14 +229,9 @@ class BookFacetJSONView(BookListView):
     def get_context_data(self, **kwargs):
         # skip normal context handling and only return count and facets
 
-        # get paginator for including number of pages, but skip other
-        # context data logic needed for full result view
-        paginator = self.get_paginator(self.object_list, self.paginate_by)
         try:
             return {
                 'total': self.object_list.count(),
-                'resultsPerPage': self.paginate_by,
-                'pages': paginator.num_pages,
                 'facets': self.object_list.get_facets(),
                 'range_facets': self.object_list.get_facets_ranges()
             }
